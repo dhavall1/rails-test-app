4a0dbce6b7af58383d8860befb4eadd5ecf8c46f
dhaval <dhaval.lokagariwar@icloud.com>
2018-04-01 06:38:33 +0000

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'welcome/home', to: 'welcome#home'
   
    get 'welcome/about', to: 'welcome#about'
+   
+   get 'root_path', to: 'pages:home'
   
 
   # Example of regular route:
